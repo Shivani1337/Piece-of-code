dc2c9b76eb2f57950815e648df6b0b0ad304dcc2
Shivani Kumari <53550705+sh1v4n1@users.noreply.github.com>
2020-01-19 11:20:00 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,1 +1,11 @@
-
+n1=int(input("Enter a number:"))
+temp=n1
+rev=0
+while(n1>0):
+    n2=n1%10
+    rev=rev*10+n2
+    n1=n1//10
+if(temp==rev):
+    print("The number is palindrome")
+else:
+    print("Not a palindrome")

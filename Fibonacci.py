9f3f1f3e7b2b64198330c4b4f2f4a38180c3ef98
Shivani Kumari <53550705+sh1v4n1@users.noreply.github.com>
2020-01-19 12:45:29 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,1 +1,16 @@
-
+n=int(input("Enter the number of terms : "))
+n1, n2 = 0, 1
+count=0
+if n<=0:
+   print("Please enter a positive number")
+elif n==1:
+   print("Fibonacci sequence of",n,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count<n:
+       print(n1)
+       num= n1 + n2
+       n1 = n2
+       n2 = num
+       count += 1

fa32945b2a05389d07af3622191c751e838ffe14
Vishal Kumar Mahto <64953843+vishalkmahto@users.noreply.github.com>
2020-10-17 17:27:41 +0530

    added queue elements

--- linked-queue.cpp
+++ linked-queue.cpp
@@ -26,3 +26,58 @@ void Insert() {
      rear = temp;
   }
 }
+
+void Delete() {
+  temp = front;
+  if (front == NULL) {
+     cout<<"Underflow"<<endl;
+     return;
+  }
+  else
+  if (temp->next != NULL) {
+     temp = temp->next;
+     cout<<"Element deleted from queue is : "<<front->data<<endl;
+     free(front);
+     front = temp;
+  } else {
+     cout<<"Element deleted from queue is : "<<front->data<<endl;
+     free(front);
+     front = NULL;
+     rear = NULL;
+  }
+}
+void Display() {
+  temp = front;
+  if ((front == NULL) && (rear == NULL)) {
+     cout<<"Queue is empty"<<endl;
+     return;
+  }
+  cout<<"Queue elements are: ";
+  while (temp != NULL) {
+     cout<<temp->data<<" ";
+     temp = temp->next;
+  }
+  cout<<endl;
+}
+int main() {
+  int ch;
+  cout<<"1) Insert element to queue"<<endl;
+  cout<<"2) Delete element from queue"<<endl;
+  cout<<"3) Display all the elements of queue"<<endl;
+  cout<<"4) Exit"<<endl;
+do {
+  cout<<"Enter your choice : "<<endl;
+  cin>>ch;
+  switch (ch) {
+     case 1: Insert();
+        break;
+     case 2: Delete();
+        break;
+     case 3: Display();
+        break;
+     case 4: cout<<"Exit"<<endl;
+        break;
+     default: cout<<"Invalid choice"<<endl;
+  }
+} while(ch!=4);
+}

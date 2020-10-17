2c5f34991814e3f4c6c5719b1b823982d8856b3b
Shivani Kumari <53550705+Shivani1337@users.noreply.github.com>
2020-10-17 13:39:27 +0530

    Create overloading.java

---
+++ overloading.java
@@ -0,0 +1,16 @@
+The main package: 
+ 
+package newp; 
+ 
+import newp.*;
+import newp.newp2.*; 
+public class main 
+{ 
+     public static void main(String[] args)
+ {         
+newp.NewClass o=new newp.NewClass();         
+o.add();         
+newp.newp2.NewClass1 i=new newp.newp2.NewClass1();         
+i.add();      
+ }     
+ } 

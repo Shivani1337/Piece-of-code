ad5fce3a0212fad4ef777ee9116df5d43d596644
Shivani Kumari <53550705+Shivani1337@users.noreply.github.com>
2020-10-17 17:30:32 +0530

    adding insertion sorting

--- sorting-operations.cpp
+++ sorting-operations.cpp
@@ -66,3 +66,31 @@ int main()
  for(int i=0; i<n; i++)
    cout<<A[i]<<" ";
 }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ int A[20], temp, n, j;
+ cout<<"Enter the size of the array: ";
+ cin>>n;
+ cout<<"Enter the elements of the array: ";
+ for(int i=0; i<n; i++)
+ {
+   cin>>A[i];
+ }
+ cout<<"\nArray after insertion sorting is: ";
+ for(int i=1; i<=n-1; i++)
+ {
+   temp= A[i];
+   j=i-1;
+   while(j>=0 && A[j]>temp)
+   {
+     A[j+1]=A[j];
+     j--;
+   }
+   A[j+1]=temp;
+ }
+ for(int i=0; i<n; i++)
+   cout<<A[i]<<" ";
+}

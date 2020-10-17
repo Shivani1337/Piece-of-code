02808f1f1026b51d9032721a2b487951d310cc67
Shivani Kumari <53550705+Shivani1337@users.noreply.github.com>
2020-10-17 17:51:24 +0530

    Create frequency-of-array.cpp

---
+++ frequency-of-array.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void findFrequency(int A[], int n)
+{
+  int freq[n];
+
+  for (int i = 0; i < n; i++)
+    if (freq[i])
+
+      printf("%d appears %d times\n", i, freq[i]);
+}
+
+int main(void)
+{
+  int A[] = { 1,1,3,3,3,2,2,2,2,3,3,2,1,2,2,2 };
+
+  int n = sizeof(A) / sizeof(A[0]);
+
+  findFrequency(A, n);
+  }
+

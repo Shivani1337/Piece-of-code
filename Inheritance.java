9727fbf983af5b0d121cd8b47ab141da847f634d
Vishal Kumar Mahto <64953843+vishalkmahto@users.noreply.github.com>
2020-10-17 17:24:41 +0530

    added hierarchical inheritance

--- Inheritance.java
+++ Inheritance.java
@@ -42,3 +42,36 @@ d.bark();
 15.	d.eat();  
 16.	}}  
 
+
+//Hierarchical Inheritance	
+class one
+{
+public void print_geek()
+{
+System.out.println("Geeks");
+}
+	}
+	
+	class two extends one
+	{	  public void print_for()
+  {
+    System.out.println("for");
+	  }
+	}
+	
+	class three extends one
+{	  /............/
+}
+	
+	// Derived class
+	public class Main
+	{
+	  public static void main(String[] args)
+	  {
+	    three g = new three();
+    g.print_geek();
+	    two t = new two();
+	    t.print_for();
+    g.print_geek();
+  }
+	}

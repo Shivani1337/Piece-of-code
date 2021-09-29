09d18202e06fc17ac09a991530ac0d2819f2ec14
Siddharth <siddharthlaldeo@gmail.com>
2021-09-29 10:16:24 +0530

    adding age.py

---
+++ age.py
@@ -0,0 +1,10 @@
+# name = input("Enter your name :")
+# age= int(input("Enter your age:"))
+# year=str((2021-age)+100)
+# print(name + " will be 100 years old in " + year + " Year")
+
+no=int(input("Enter your number "))
+if no%2==0:
+    print(str(no) + " is even no")
+else:l
+    print(str(no) +" is odd no")

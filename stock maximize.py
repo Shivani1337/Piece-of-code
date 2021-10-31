df7c1cb3dc4dc8876959d21fa3330357b24dc1bc
Vanisha Vaish <vanisha.vaish@gmail.com>
2021-10-31 20:42:10 +0530

    Create stock maximize.py

---
+++ stock maximize.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'stockmax' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY prices as parameter.
+#
+
+def stockmax(prices):
+    # Write your code here
+    profit = 0
+    max = 0
+    for i in range(len(prices)-1,-1,-1):
+        if(prices[i] >= max):
+            max = prices[i]
+        profit += max - prices[i]
+        print(profit)
+    return profit
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        prices = list(map(int, input().rstrip().split()))
+
+        result = stockmax(prices)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
